874986b5afe83017b302cd8196fc2a23bf29ac8c
Josh Duff <me@JoshDuff.com>
2016-02-10 21:21:29 -0600

    Make it so

---
+++ index.js
@@ -0,0 +1,47 @@
+module.exports = function magicalDomThingy(stateRouter) {
+	const currentDomApis = {}
+	const currentAttachListeners = []
+	const currentDetachListeners = []
+
+	function attachToState({ state, domApi }) {
+		currentDomApis[state.name] = domApi
+		currentAttachListeners.forEach(fn => fn(domApi))
+	}
+
+	function detachFromState({ state }) {
+		currentDetachListeners.forEach(fn => fn(currentDomApis[state.name]))
+		delete currentDomApis[state.name]
+	}
+
+	stateRouter.on('afterCreateState', attachToState)
+	stateRouter.on('afterResetState', attachToState)
+
+	stateRouter.on('beforeResetState', detachFromState)
+	stateRouter.on('beforeDestroyState', detachFromState)
+
+	function addDomApiAttachListener(attachListener) {
+		Object.keys(currentDomApis).forEach(stateName => attachListener(currentDomApis[stateName]))
+
+		return addToArrayAndReturnRemover(currentAttachListeners, attachListener)
+	}
+
+	function addDomApiDetachListener(detachListener) {
+		return addToArrayAndReturnRemover(currentDetachListeners, detachListener)
+	}
+
+	return {
+		addDomApiAttachListener: addDomApiAttachListener,
+		addDomApiDetachListener: addDomApiDetachListener
+	}
+}
+
+function addToArrayAndReturnRemover(ary, thingy) {
+	ary.push(thingy)
+
+	return function removeListener() {
+		var index = ary.indexOf(thingy)
+		if (index !== -1) {
+			ary.splice(index, 1)
+		}
+	}
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,26 @@
-# asr-active-state-watcher
-[![NPM](https://nodei.co/npm/asr-active-state-watcher.png)](https://nodei.co/npm/asr-active-state-watcher/)
+So you start with an [abstract-state-router](https://github.com/TehShrike/abstract-state-router), right?
+
+When you call `addDomApiAttachListener`, your callback will be called for *every currently active state*, as well as every state that becomes active after that point.
+
+When you call `addDomApiDetachListener`, your callback will be called every time a state is navigated away from.
+
+Your callbacks will be passed one argument: the DOM API object for the state.
+
+```js
+
+const stateWatcher = makeAsrStateWatcher(stateRouter)
+
+const removeAttachListener = magicalDomThingy.addDomApiAttachListener(domApi => {
+	domApi.get() // the starting state, probably!
+})
+
+const removeDetachListener = magicalDomThingy.addDomApiDetachListener(domApi => {
+	domApi.get() // the final state, presumably!
+})
+
+function cleanUp() {
+	removeAttachListener()
+	removeDetachListener()
+}
+
+```
